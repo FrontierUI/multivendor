b8a9c5ac91b024dc6429709c7ce5eed3089f4a90
FrontierUI <uifrontier95@gmail.com>
2025-09-04 23:12:11 +0500

    home layout

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <div className="flex border-t justify-between font-medium p-6">
+      <div className="centy gap-2">
+        <p>funroad, Inc.</p>
+      </div>
+    </div>
+  );
+};
---
+++ src/app/(home)/navbar-sidebar.tsx
@@ -0,0 +1,63 @@
+import React, { ReactNode } from 'react';
+import Link from 'next/link';
+
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from '@/components/ui/sheet';
+import { ScrollArea } from '@/components/ui/scroll-area';
+
+interface NavbarItem {
+  href: string;
+  children: ReactNode;
+}
+
+interface Props {
+  items: NavbarItem[];
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export const NavbarSidebar = ({ items, open, onOpenChange }: Props) => {
+  return (
+    <Sheet open={open} onOpenChange={onOpenChange}>
+      <SheetContent side="left" className="p-0 transition-none">
+        <SheetHeader className="p-4 border-b">
+          <SheetTitle>Menu</SheetTitle>
+        </SheetHeader>
+
+        <ScrollArea className="flex flex-col overflow-y-auto h-full pb-2">
+          {items.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              onClick={() => onOpenChange(false)}
+              className="w-full text-left p-4 centy hover:bg-black hover:text-white font-medium text-base"
+            >
+              {item.children}
+            </Link>
+          ))}
+
+          <div className="border-t">
+            <Link
+              href={'/sign-in'}
+              onClick={() => onOpenChange(false)}
+              className="w-full text-left p-4 centy hover:bg-black hover:text-white font-medium text-base"
+            >
+              Log in
+            </Link>
+            <Link
+              href={'/sign-up'}
+              onClick={() => onOpenChange(false)}
+              className="w-full text-left p-4 centy hover:bg-black hover:text-white font-medium text-base"
+            >
+              Start Selling
+            </Link>
+          </div>
+        </ScrollArea>
+      </SheetContent>
+    </Sheet>
+  );
+};
